34bef5e6d748f5e9d5bd5fe145976f18eb6300b7
Leila-codes <leila.richardson-noyes@protonmail.com>
2023-11-04 17:36:29 +0000

    04-11-2023 - Added filter transform function.

---
+++ transform/filter.go
@@ -0,0 +1,15 @@
+package transform
+
+type FilterFunc[IN interface{}] func(IN) bool
+
+// Filter - Applies the given filter to each event received from input.
+// If the filter returns true, the event is output to the returned channel.
+// Else, the event is ignored.
+func Filter[IN interface{}](input chan IN, filter FilterFunc[IN]) chan IN {
+	out := make(chan IN)
+	for {
+		if event := <-input; filter(event) {
+			out <- event
+		}
+	}
+}
