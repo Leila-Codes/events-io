094f47135a4e108bd92728ad6e07cbb0127b2479
Leila Richardson-Noyes <leila.richardson-noyes@protonmail.com>
2024-06-17 16:49:01 +0100

    Update filter.go
    Updated filter.go to be async - as nature intended.

--- transform/filter.go
+++ transform/filter.go
@@ -2,14 +2,19 @@ package transform
 
 type FilterFunc[IN interface{}] func(IN) bool
 
+func doFilter[IN interface{}(input <-chan IN, output chan<- IN, filter FilterFunc[IN]) {
+	for event := range input {
+		if filter(event) {
+			output <- event
+		}
+	}
+}
+
 // Filter - Applies the given filter to each event received from input.
 // If the filter returns true, the event is output to the returned channel.
 // Else, the event is ignored.
 func Filter[IN interface{}](input <-chan IN, filter FilterFunc[IN]) chan IN {
 	out := make(chan IN)
-	for {
-		if event := <-input; filter(event) {
-			out <- event
-		}
-	}
+	go doFilter(input, out, filter)
+	return out
 }
